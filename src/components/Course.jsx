f11b7095dc9073b5f4aebb2eefe49f6b13b0a98e
Hossam Ali <hossamali130@gmail.com>
2022-08-31 03:21:10 +0200

    fixing phase 1

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 export default function Course(props) {
-  let course = props.course;
   let stars = [];
-  for (let i = 0; i < Math.floor(course.rating); i++) {
+  const { image, title, rating, instructors, price } = props.course;
+  for (let i = 0; i < Math.floor(rating); i++) {
     stars.push(
       <FontAwesomeIcon
         icon={faStar}
@@ -16,27 +16,25 @@ export default function Course(props) {
   }
   return (
     <a href="##">
-      <img src={course.image} alt={course.title} className="border" />
-      <p className="font-bold text-gray-900">{course.title}</p>
+      <img src={image} alt={title} className="border" />
+      <p className="font-bold text-gray-900">{title}</p>
       <p className="text-xs text-gray-500 whitespace-nowrap overflow-hidden text-ellipsis max-w-[10rem]">
-        {course.instructors.map((instructor, idx) => {
+        {instructors?.map((instructor, idx) => {
           return `${idx ? ", " : ""} ${instructor.name}`;
         })}
       </p>
-      <span className="text-yellow-600 font-bold">
-        {course.rating.toFixed(1)}
-      </span>
+      <span className="text-yellow-600 font-bold">{rating.toFixed(1)}</span>
       {stars.map((star) => {
         return star;
       })}
-      {course.rating > Math.floor(course.rating) && (
+      {rating > Math.floor(rating) && (
         <FontAwesomeIcon
           icon={faStarHalfAlt}
           size="xs"
           className="text-yellow-500 pl-1"
         />
       )}
-      <p className="font-bold text-gray-900">E£{course.price}</p>
+      <p className="font-bold text-gray-900">E£{price}</p>
     </a>
   );
 }
